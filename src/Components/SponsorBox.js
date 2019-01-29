5f5bed324b4250010a7ef21e77ac0cc53770b6ff
SMB <sethmichaelbrown@gmail.com>
2019-01-29 10:53:04 -0700

    Removed login and sign up buttons

--- src/Components/SponsorBox.js
+++ src/Components/SponsorBox.js
@@ -11,7 +11,7 @@ const SponsorBox = (props) => {
         <div className="col-md-6 col-centered offset-md-3">
           <img className='' src={logo} alt="bts-logo" width="303" height="130" />
         </div>
-        <div className="row mt-2">
+        <div className="row mt-3">
           <div className="col-md-12">
             <p>
               The concept for Bus to Show was conceived by our founder several years in the future, and then planted in the mind of his younger self (2007) through inter-temporal-telepathy. Bus to Show is, at its core, designed to save the lives of a few future political and spiritual leaders, who would otherwise have been killed in their youth by impaired drivers on their way home from concerts. At the same time, Bus to Show works to reduce the amount of fuel consumption that results from events, which, in turn, will delay the destruction of the Earth long enough for the saved leaders to come of-age and implement their plans for reaching a sustained equilibrium between industry and environment.
@@ -21,10 +21,6 @@ const SponsorBox = (props) => {
           </p>
           </div>
         </div>
-          <div className="col-md-6 col-centered offset-md-3">
-            <button type="button" className="btn btn-primary btn-lg btn-block">Donate</button>
-            <button type="button" className="btn btn-secondary btn-lg btn-block">My Cart</button>
-          </div>
         </div>
       </div>
 
