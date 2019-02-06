7ac7fb9ad816f97fd3e35fafe6359b3eaf6f1bc4
SMB <sethmichaelbrown@gmail.com>
2019-02-06 15:17:58 -0700

    adding zoom feature to loading page

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../App.css';
+import logo from '../Images/Logos/bts-logo-white.png'
+import Zoom from 'react-reveal/Zoom';
 
 
 const Loading = (props) => {
@@ -9,7 +11,9 @@ const Loading = (props) => {
     <div className='Loading loading-image'>
       <div className="row">
         <div className="col-md-12 loading-header">
-          <img src="../Images/Logos/bts-logo-white.png" alt="bts-logo"/>
+          <Zoom>
+            <img className='render-opacity' src={logo} alt="bts-logo" height='156' width='350' />
+          </Zoom>
         </div>
       </div>
     </div>
