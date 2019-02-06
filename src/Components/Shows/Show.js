9555e234d29a6b60147406a10780fd36bb311c87
SMB <sethmichaelbrown@gmail.com>
2019-02-06 12:22:41 -0700

    Adding supporting artist to list

--- src/Components/Shows/Show.js
+++ src/Components/Shows/Show.js
@@ -6,26 +6,35 @@ import MediaQuery from 'react-responsive';
 const Shows = (props) => {
   const filterString = props.filterString.toLowerCase()
   let filterShows = props.shows.filter(show => show.headliner.toLowerCase().includes(filterString))
-  
-  if(filterShows.length === 0){
+
+  if (filterShows.length === 0) {
     filterShows = props.shows.filter(show => show.venue.toLowerCase().includes(filterString))
   }
 
-  console.log(filterShows)
+  const support2 = filterShows.filter(item => item.support2)
+
+  console.log('Support2', support2)
 
   return (
     <div className='Shows'>
       {filterShows.length > 0 ? filterShows.map(show =>
         <li className="list-group-item highlightOnHover show-list-item" key={show.id} id={show.id}>
           <div className="row" id={show.id}>
-          <MediaQuery minWidth={768}>
-            <div className="col-md-3 list-item-font" id={show.id}>{show.date} <br /> {moment(show.date, "MM-DD-YYYY").format("dddd")}</div>
-            <div className="col-md-7 list-item-font" id={show.id}><strong>{show.headliner}</strong> <br />{show.venue}</div>
-          </MediaQuery>
-          <MediaQuery maxWidth={768}>
-            <div className="col-md-3 list-item-font" id={show.id}>{show.date} <br /> {moment(show.date, "MM-DD-YYYY").format("dddd")}</div>
-            <div className="col-md-7 list-item-font" id={show.id}><strong>{show.headliner}</strong> <br />{show.venue}</div>
-          </MediaQuery>
+            <MediaQuery minWidth={768}>
+              <div className="col-md-3 list-item-font" id={show.id}>{show.date} <br /> {moment(show.date, "MM-DD-YYYY").format("dddd")}</div>
+              <div className="col-md-7 list-item-font" id={show.id}>
+                <strong>{show.headliner}</strong> <br /> 
+                  {show.support1 ? <React.Fragment> with {show.support1} <br /> </React.Fragment> : ''} 
+                  {show.support2 ? <React.Fragment> and more! <br /> </React.Fragment> : ''}    
+                  {show.venue}
+              </div>
+            </MediaQuery>
+            <MediaQuery maxWidth={768}>
+              <div className="col-md-3 list-item-font" id={show.id}>{show.date} <br /> {moment(show.date, "MM-DD-YYYY").format("dddd")}</div>
+              <div className="col-md-7 list-item-font" id={show.id}>
+                <strong>{show.headliner}</strong> <br />{show.venue}
+              </div>
+            </MediaQuery>
             <button
               id={show.id}
               // onClick={props.addBorder}
