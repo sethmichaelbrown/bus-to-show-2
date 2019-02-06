4a1b5f0ab0de360793c59b1f2de891a051086068
SMB <sethmichaelbrown@gmail.com>
2019-02-06 15:44:13 -0700

    general styling/ cleaning

--- src/Components/Shows/ShowList.js
+++ src/Components/Shows/ShowList.js
@@ -12,22 +12,22 @@ const Shows = (props) => {
         <div className="list-group">
           <div className="list-group-item show-header">
 
-              <div className="row">
-              <div style={{textAlign:"center"}} className="col-md-3" onClick={props.sortByDate}>
-              <button type="button" className="btn btn-outline-light sort-btn"><strong>Date  </strong>
-              <i className={ props.sortedByDate? "fa fa-sort-down": ""}></i>
-              </button>
+            <div className="row">
+              <div style={{ textAlign: "center" }} className="col-md-3" onClick={props.sortByDate}>
+                <button type="button" className="btn btn-outline-light sort-btn"><strong>Date  </strong>
+                  <i className={props.sortedByDate ? "fa fa-sort-down" : ""}></i>
+                </button>
 
               </div>
-              <div style={{textAlign:"center"}} className="col-md-3" onClick={props.sortByArtist}>
-              <button type="button" className="btn btn-outline-light sort-btn"><strong>Event  </strong>
-              <i className={props.sortedByArtist? "fa fa-sort-down": ""}></i>
-              </button>
+              <div style={{ textAlign: "center" }} className="col-md-3" onClick={props.sortByArtist}>
+                <button type="button" className="btn btn-outline-light sort-btn"><strong>Event  </strong>
+                  <i className={props.sortedByArtist ? "fa fa-sort-down" : ""}></i>
+                </button>
 
               </div>
               <div className="col-md-5 mb-3 ml-1">
                 <form className="form-inline ml-3 my-4 my-lg-0">
-                  <input onKeyUp={props.searchShows} className="form-control mr-sm-2" type="search" placeholder="Search Events or Venues..." aria-label="Search"></input>
+                  <input onKeyUp={props.searchShows} className="form-control search-bar mr-sm-2" type="search" placeholder="Search Events or Venues..." aria-label="Search"></input>
                 </form>
               </div>
 
