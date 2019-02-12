b925c32df99a717aad01919785d7cece3044169f
SMB <sethmichaelbrown@gmail.com>
2019-02-11 19:12:57 -0700

    Mobile showList to showDetail working

--- src/Components/DetailCartView.js
+++ src/Components/DetailCartView.js
@@ -144,124 +144,121 @@ const DetailCartView = (props) => {
             </li>
           </ul>
           <div className="tab-content" id="mobile-tab-content">
-            <div className="mobile-showList tab-pane fade show active" id="showList" role="tabpanel" aria-labelledby="showList-tab">
-              {props.shows && props.displayShowList ?
-                <ShowList
-                  addBorder={props.addBorder}
-                  displayShow={props.displayShow}
-                  filterString={props.filterString}
-                  handleWarning={props.handleWarning}
-                  inCart={props.inCart}
-                  searchShows={props.searchShows}
-                  shows={props.shows}
-                  mobileShowsExpandClick={props.mobileShowsExpandClick}
-                  sortByArtist={props.sortByArtist}
-                  sortByDate={props.sortByDate}
-                  sortedByArtist={props.artistIcon}
-                  sortedByDate={props.dateIcon}
-                  ticketsAvailable={props.ticketsAvailable} />
-                :
-                ''
-              }
-            </div>
-            <div className="mobile-showDetailView tab-pane fade" id="showDetails" role="tabpanel" aria-labelledby="showDetails-tab">
-              {props.displayShow ?
-                <ShowDetailView
-                  addToCart={props.addToCart}
-                  displayAddBtn={props.displayAddBtn}
-                  displayBorder={props.displayBorder}
-                  displayCart={props.displayCart}
-                  displayQuantity={props.displayQuantity}
-                  displayShow={props.displayShow}
-                  displaySuccess={props.displaySuccess}
-                  displayViewCartBtn={props.displayViewCartBtn}
-                  displayWarning={props.displayWarning}
-                  findDiscountCode={props.findDiscountCode}
-                  inCart={props.inCart}
-                  mobileShowsExpandClick={props.mobileShowsExpandClick}
-                  pickupLocations={props.pickupLocations}
-                  pickupLocationId={props.pickupLocationId}
-                  returnToShows={props.returnToShows}
-                  selectPickupLocationId={props.selectPickupLocationId}
-                  selectTicketQuantity={props.selectTicketQuantity}
-                  ticketsAvailable={props.ticketsAvailable}
-                  ticketQuantity={props.ticketQuantity}
-                  updateDiscountCode={props.updateDiscountCode}
-                  totalCost={props.totalCost}
-                  viewCart={props.viewCart} />
-                :
-                <div className="noShowSelected">
-                  <div className="row nothing-in-cart-text">
-                    <div className="col-md-12 mt-3">
-                      <h1>Nothing Selected!</h1>
-                    </div>
-                    <div className="col-md-12 mt-3">
-                      <img
-                        className='nothing-in-cart-image'
-                        src={logo}
-                        alt="bts-logo"
-                        width="233"
-                        height="100" />
-                    </div>
-                  </div>
+
+            {props.shows && props.displayShowList ?
+              <ShowList
+                addBorder={props.addBorder}
+                displayShow={props.displayShow}
+                filterString={props.filterString}
+                handleWarning={props.handleWarning}
+                inCart={props.inCart}
+                searchShows={props.searchShows}
+                shows={props.shows}
+                mobileShowsExpandClick={props.mobileShowsExpandClick}
+                sortByArtist={props.sortByArtist}
+                sortByDate={props.sortByDate}
+                sortedByArtist={props.artistIcon}
+                sortedByDate={props.dateIcon}
+                ticketsAvailable={props.ticketsAvailable} />
+              :
+              ''
+            }
+          </div>
+          {props.displayShow ?
+            <ShowDetailView
+              addToCart={props.addToCart}
+              displayAddBtn={props.displayAddBtn}
+              displayBorder={props.displayBorder}
+              displayCart={props.displayCart}
+              displayQuantity={props.displayQuantity}
+              displayShow={props.displayShow}
+              displaySuccess={props.displaySuccess}
+              displayViewCartBtn={props.displayViewCartBtn}
+              displayWarning={props.displayWarning}
+              findDiscountCode={props.findDiscountCode}
+              inCart={props.inCart}
+              mobileShowsExpandClick={props.mobileShowsExpandClick}
+              pickupLocations={props.pickupLocations}
+              pickupLocationId={props.pickupLocationId}
+              returnToShows={props.returnToShows}
+              selectPickupLocationId={props.selectPickupLocationId}
+              selectTicketQuantity={props.selectTicketQuantity}
+              ticketsAvailable={props.ticketsAvailable}
+              ticketQuantity={props.ticketQuantity}
+              updateDiscountCode={props.updateDiscountCode}
+              totalCost={props.totalCost}
+              viewCart={props.viewCart} />
+            :
+            <div className="noShowSelected">
+              <div className="row nothing-in-cart-text">
+                <div className="col-md-12 mt-3">
+                  <h1>Nothing Selected!</h1>
                 </div>
-              }
-            </div>
-            <div className="tab-pane fade" id="cart" role="tabpanel" aria-labelledby="cart-tab">
-              {props.inCart.length > 0 && props.displayCart ?
-                <Cart
-                  afterDiscountObj={props.afterDiscountObj}
-                  checked={props.checked}
-                  confirmedRemove={props.confirmedRemove}
-                  closeAlert={props.closeAlert}
-                  displayConfirmRemove={props.displayConfirmRemove}
-                  displayWarning={props.displayWarning}
-                  findDiscountCode={props.findDiscountCode}
-                  firstBusLoad={props.firstBusLoad}
-                  getPickupParty={props.getPickupParty}
-                  handleCheck={props.handleCheck}
-                  handleSubmit={props.handleSubmit}
-                  inCart={props.inCart}
-                  lastDepartureTime={props.lastDepartureTime}
-                  makePurchase={props.makePurchase}
-                  pickupLocationId={props.pickupLocationId}
-                  pickupLocations={props.pickupLocations}
-                  pickupParties={props.pickupParties}
-                  purchase={props.purchase}
-                  purchaseClick={props.purchaseClick}
-                  purchasePending={props.purchasePending}
-                  purchaseSuccessful={props.purchaseSuccessful}
-                  quantityChange={props.quantityChange}
-                  removeFromCart={props.removeFromCart}
-                  returnToShows={props.returnToShows}
-                  shows={props.shows}
-                  showsInCart={props.inCart}
-                  ticketQuantity={props.ticketQuantity}
-                  totalCost={props.totalCost}
-                  updateDiscountCode={props.updateDiscountCode}
-                  updatePurchaseField={props.updatePurchaseField}
-                  validated={props.validated}
-                  validatedElements={props.validatedElements} />
-                :
-                <div className="noShowSelected">
-                  <div className="row nothing-in-cart-text">
-                    <div className="col-md-12 mt-3">
-                      <h1>Nothing in Cart!</h1>
-                    </div>
-                    <div className="col-md-12 mt-3">
-                      <img
-                        className='nothing-in-cart-image'
-                        src={logo}
-                        alt="bts-logo"
-                        width="233"
-                        height="100" />
-                    </div>
-                  </div>
+                <div className="col-md-12 mt-3">
+                  <img
+                    className='nothing-in-cart-image'
+                    src={logo}
+                    alt="bts-logo"
+                    width="233"
+                    height="100" />
                 </div>
-              }
+              </div>
+            </div>
+          }
+        </div>
+        {props.inCart.length > 0 && props.displayCart ?
+          <Cart
+            afterDiscountObj={props.afterDiscountObj}
+            checked={props.checked}
+            confirmedRemove={props.confirmedRemove}
+            closeAlert={props.closeAlert}
+            displayConfirmRemove={props.displayConfirmRemove}
+            displayWarning={props.displayWarning}
+            findDiscountCode={props.findDiscountCode}
+            firstBusLoad={props.firstBusLoad}
+            getPickupParty={props.getPickupParty}
+            handleCheck={props.handleCheck}
+            handleSubmit={props.handleSubmit}
+            inCart={props.inCart}
+            lastDepartureTime={props.lastDepartureTime}
+            makePurchase={props.makePurchase}
+            pickupLocationId={props.pickupLocationId}
+            pickupLocations={props.pickupLocations}
+            pickupParties={props.pickupParties}
+            purchase={props.purchase}
+            purchaseClick={props.purchaseClick}
+            purchasePending={props.purchasePending}
+            purchaseSuccessful={props.purchaseSuccessful}
+            quantityChange={props.quantityChange}
+            removeFromCart={props.removeFromCart}
+            returnToShows={props.returnToShows}
+            shows={props.shows}
+            showsInCart={props.inCart}
+            ticketQuantity={props.ticketQuantity}
+            totalCost={props.totalCost}
+            updateDiscountCode={props.updateDiscountCode}
+            updatePurchaseField={props.updatePurchaseField}
+            validated={props.validated}
+            validatedElements={props.validatedElements} />
+          :
+          <div className="noShowSelected">
+            <div className="row nothing-in-cart-text">
+              <div className="col-md-12 mt-3">
+                <h1>Nothing in Cart!</h1>
+              </div>
+              <div className="col-md-12 mt-3">
+                <img
+                  className='nothing-in-cart-image'
+                  src={logo}
+                  alt="bts-logo"
+                  width="233"
+                  height="100" />
+              </div>
             </div>
           </div>
-        </div>
+        }
+
+
 
       </MediaQuery>
       {/* // End Mobile View */}
